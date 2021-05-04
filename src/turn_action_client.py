743c5f9eb09d52b44988eaf181b4d2e828798cf0
tristandu <43992693+tristandu@users.noreply.github.com>
2021-05-04 15:45:53 +0100

    Update turn_action_client.py

--- src/turn_action_client.py
+++ src/turn_action_client.py
@@ -3,20 +3,24 @@ from __future__ import print_function
 import rospy 
 import actionlib
 import line_follower_turtlebot.msg
-from std_msgs.msg import Int32
+from std_msgs.msg import String,Int32
 
 class TurnClient():
 
     def __init__(self):
         self.sub_mode = rospy.Subscriber("/mode", Int32, self.cbMode)
+        self.sub_next_turn=rospy.Subscriber("/next_turn",String, self.cbNextTurn)
         self.last_msg = 0
+        self.goal=None
         self.client = actionlib.SimpleActionClient('turn_action_server', line_follower_turtlebot.msg.TurnAction)
         self.client.wait_for_server()
+        
+    def cbNextTurn(self,msg):
+        self.goal=self.goal = line_follower_turtlebot.msg.TurnGoal(turn_direction=msg.data)
 
 
     def cbMode(self, msg):
         if (msg.data==1 and self.last_msg!=1):
-            self.goal = line_follower_turtlebot.msg.TurnGoal(turn_direction="Right")
             self.client.send_goal(self.goal)
             self.client.wait_for_result()
             self.res=self.client.get_result()
